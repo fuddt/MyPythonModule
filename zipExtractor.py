e58628ad4ca41d3f7c717bd3168b7492d6673428
shirotabi <64385784+fuddt@users.noreply.github.com>
2023-01-02 18:30:20 +0900

    zipExtractor.pyの説明文追加

--- zipExtractor.py
+++ zipExtractor.py
@@ -3,6 +3,14 @@ import os
 import glob
 
 def zipExtractorMeantForInDir(dir_with_zip,to_dir):
+    """
+    summary:
+    ディレクトリ内のzipファイルをすべて展開します。
+
+    Args:
+        dir_with_zip (str): zipファイルが入ったディレクトリ 
+        to_dir (srt): 展開先ディレクトリ
+    """
     dir_with_zip = os.path.join(dir_with_zip,'*.zip')
     filepaths = glob.glob(dir_with_zip)
     for filepath in filepaths:
